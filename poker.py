a68423233467081cb2caefd502dc9cc47c9541e6
Tom <tomburgess24@gmail.com>
2021-03-02 13:57:06 +0000

    Update poker.py
    made some nicer methods and removed many bad ones
    now can detect all outcomes, just need precision for same results but different cards (eg two pair with different kicker, or different pair(s))

--- poker.py
+++ poker.py
@@ -1,4 +1,7 @@
 import random
+# note there may be an error when there's a straight and a flush
+# in that the straight will be identified yet the flush will be shown for its cards...
+
 # strings from which to generate the 52 card deck
 cards = "23456789TJQKA"
 suits = "DCHS"
@@ -53,19 +56,15 @@ while cardsPtr.value != -1:
 # instead of shuffling you can just generate a random number from 0 to 51 and select from the ordered deck
 # you can then use a mutable ordered deck and take out each choice
 mutableOrderedDeck = deck.copy()
-def detectWinCategory():
-    ["high card", "pair", "two pair", "three of a kind", "straight", 
-     "flush", "full house", "four of a kind", "straight flush"]
-    # high card condition: no pairs, no straights nor flushes
-    # note you only need to search for those three categories above mostly
-def sames(sevenCards, ofAKindNum): # if three/four of a kind this does NOT return the pair
+# sames() just returns the number of occurences of each card from 2 to Ace
+# it can be used to find pairs, 3oaK, 4oaK and full house
+def sames(sevenCards, ofAKindNum): 
     cards = "23456789TJQKA"
     found = [0]*len(cards)
-    pairCount = 0
     cardsIndex = 0
     output = ""
+    # making an array of found counts for what cards appear for player
     for c in cards:
-        pairCount = 0
         for s in sevenCards: # suits can be ignored implicitly
             if s==c:
                 found[cardsIndex] = found[cardsIndex] + 1
@@ -74,18 +73,64 @@ def sames(sevenCards, ofAKindNum): # if three/four of a kind this does NOT retur
     for f in found:
         if f==ofAKindNum:
             output = output + cards[count]
-            count = count + 1
-    if len(output) > 1: # note this will never happen for 4 of a kind but could for three of a kind (2)
-        if ofAKindNum==2:
-            return output[-1] + output[-2] # two pair
-        elif ofAKindNum==3:
-            return output[0] # three of a kind
-        elif ofAKindNum==4:
-            return output[0] # four of a kind
-    elif len(output) == 1: #pair
-        return output
-    else:
-        return ""
+        count = count + 1
+    return output[::-1]
+def straightCheck(sevenCards): 
+    cards = "A23456789TJQKA"
+    found = [0]*len(cards)
+    linked = 0
+    cardsIndex = 0
+    output = ""
+    finalCardIndex = -1
+    for c in cards:
+        for s in sevenCards: # suits can be ignored implicitly
+            if s==c:
+                found[cardsIndex] = found[cardsIndex] + 1
+        cardsIndex = cardsIndex + 1
+    for f in range(0,len(found)):
+        if cards[f]=="J":
+            break # because this is exceeding the highgest straight
+        linked = 0
+        for c in range(0,5):
+            if found[f + c]>0:
+                linked = linked + 1
+                if linked == 5:
+                    finalCardIndex = f + c
+                    #print("KABOOM")
+    #print(found)
+    output = ""
+    if finalCardIndex != -1:
+        output = cards[finalCardIndex-4:finalCardIndex+1]
+    #print(output)
+    return output[::-1]
+def flushCheck(sevenCards): # if three/four of a kind this does NOT return the pair
+    cards = "23456789TJQKA"
+    suits = "DCHS"
+    suitCount = [0]*len(suits)
+    for c in sevenCards:
+        for s in range(0,len(suits)):
+            if c==suits[s]:
+                suitCount[s] = suitCount[s] + 1
+    flushFound = False
+    flushSuit = ""
+    #print(suitCount)
+    for a in suitCount:
+        if a > 4:
+            flushFound = True
+            flushSuit = suits[suitCount.index(a)]
+    #print(flushSuit)
+    # find the highest of flush suit
+    captured = "" # cards that are suited in flush
+    for c in range(0,len(sevenCards)):
+        if sevenCards[c]==flushSuit:
+            captured = captured + sevenCards[c-1]
+    #print(captured)
+    ordCapt = "" # ordered version of flush cards
+    for a in reversed(cards):
+        for b in captured:
+            if a==b:
+                ordCapt = ordCapt + a
+    return ordCapt[:5]
 class Robot:
     cards = "NONE" # empty
     def __init__(self, cardOne, cardTwo):
@@ -123,14 +168,14 @@ def addRobot(hisCards):
     global table
     if hisCards=="GENERATE":
         robot = Robot(newCardFromDeck(), newCardFromDeck())
-        print("ROBOT ADDED WITH GENERATED CARDS")
+        #print("ROBOT ADDED WITH GENERATED CARDS")
         table.append(robot)
     elif hisCards!="GENERATE":
         robot = Robot(hisCards[0] + hisCards[1], hisCards[2] + hisCards[3])
         mutableOrderedDeck.remove(hisCards[0] + hisCards[1])
         mutableOrderedDeck.remove(hisCards[2] + hisCards[3])
         table.append(robot)
-        print("ROBOT ADDED WITH CUSTOM CARDS")
+        #print("ROBOT ADDED WITH CUSTOM CARDS")
         #robot.showCards()
 dealt = []
  
@@ -150,17 +195,6 @@ def deal():
         # river
         dealt.append(newCardFromDeck())
         #print(dealt)
-            
-def whoWon(): # this assumes betting is over, so 5 cards needed on dealt
-    global dealt
-    out = []
-    while len(dealt) != 5:
-        deal()
-    #print(dealt)
-    out.append(dealt)
-    for a in table:
-        #print(a.showCards())
-        out.append(a.showCards)
 
 # i'm going to create a function for every "victory category" *pair, two pair, flush etc"
 # that returns a value indicating whether it has been detected
@@ -169,95 +203,104 @@ def whoWon(): # this assumes betting is over, so 5 cards needed on dealt
 # returns the value of the highest pair
 
 # ---game simulation---
-'''
-gameState = "empty"
-while True:
-    print("There are " + str(len(table)) + " players and the game is in state: " + gameState)
-    for p in table:
-        print(p.name, end = " ")
-    print("")
-    for p in table:
-        p.showCards()
-    a = input("\nEnter command: ")
-    if a=="add":
-        a = input("custom (c) or robot (r)?")
-        if a=="r":
-            addRobot("GENERATE")
-        elif a=="c": # custom, must be no prior humans or robots because i need to select cards
-            a = input("Enter cards (AHKC == ace of hearts, king of clubs): ")
-            # no error detection for input as it is cumbersome. must be uppcase and format in here
-            addRobot(a)
-            mutableOrderedDeck.remove(a[0] + a[1])
-            mutableOrderedDeck.remove(a[2] + a[3])
-    elif a=="rem": #(remove)
-        a = input("enter their table position: (up to " + str(len(table)))
-        a = input("")
-    elif a=="dea": # deal
-        deal()
-    elif a=="dec": # deck (show it)
-        print(mutableOrderedDeck)
-        print(deck)
-    elif a=="res": # restart
-        print(mutableOrderedDeck)
-    elif a=="win": # apply win detections
-        for p in table:
-            x = p.cards
-            y = ""
-            y = y.join(dealt)
-            checkers = [sames(x + y, 2), sames(x + y, 2), sames(x + y, 3), sames(x + y, 4)]
-            if len(checkers[0])==1:
-                print(p.name + " has one pair")
-            elif len(checkers[1])>1:
-                print(p.name + " has a two pair")
-            elif len(checkers[2])>0:
-                print(p.name + " has a three of a kind")
-            elif len(checkers[3])==1:
-                print(p.name + " has a four of a kind")
-'''
-# prints what every player on the table has (intent)
-def resCheck():
-    out = ""
-    pCount = 0
-    for p in table:
-        pCount = pCount + 1
-        x = p.cards
-        y = ""
-        y = y.join(dealt)
-        checkers = [sames(x + y, 2), sames(x + y, 3), sames(x + y, 4)] # pair/two pair, 3oaK, 3oaK
-        if len(checkers[0])==1:
-            out += "Player " + str(pCount) + " has one pair\n" + checkers[0]
-        elif len(checkers[0])>1:
-            out += "Player " + str(pCount) + " has a two pair\n" + checkers[0] + checkers[1]
-        elif len(checkers[1])>0:
-            out += "Player " + str(pCount) + " has a three of a kind\n" + checkers[0]
-        elif len(checkers[2])==1:
-            out += "Player " + str(pCount) + " has a four of a kind\n" + checkers[0]
-    #print(out)
+# returns the score for each hand for how good it is according to high cards standard
+def handRanks(allHands5):
+    betterCount = [0]*len(allHands5)
+    n = 0
+    for h in allHands5:
+        for h2 in allHands5:
+            if h==h2:
+                continue
+            betterCount[n] += higher(h2, h)
+        n += 1
+    print(betterCount)
+def higher(cards5one, cards5two):
+    x = ""
+    y = ""
+    for n in range(1,6):
+        x = x + highest(cards5one, n)
+        y = y + highest(cards5two, n)
+    if x==y:
+        return "same"
+    Ato2 = "AKQJT98765432"
+    for n in range(0,len(cards5one)):
+        a = x[n]
+        b = y[n]
+        if Ato2.index(a) < Ato2.index(b):
+            return 0
+        elif Ato2.index(a) > Ato2.index(b):
+            return 1
+        elif Ato2.index(a) == Ato2.index(b):
+            continue   
+# get the highgest or nth high number from a string of unordered cards
+# very useful method
+def highest(cardsString, n):
+    cards = "AKQJT98765432"
+    found = ""
+    for x in cards:
+        for y in cardsString:
+            if x==y:
+                found += x
+    #print(cardsString)
+    return found[n-1]
+    
+def handCategory(hand7):
+    flush = len(flushCheck(hand7))==5
+    straight = len(straightCheck(hand7))==5
+    four = len(sames(hand7, 4))==1
+    three = len(sames(hand7, 3))>0 # used for full house too...
+    twop = len(sames(hand7, 2))==2
+    two = len(sames(hand7, 2))==1
+    # note straight flush has awkward conditions
+    out = "high card"
+    if two:
+        out = "pair"
+    elif twop:
+        out = "two pair"
+    elif three and (two or twop):
+        out = "full house"
+    elif three and not (two or twop):
+        out = "three of a kind"
+    elif four:
+        out = "four of a kind"
+    if straight:
+        out = "straight"
+    elif flush:
+        out = "flush"
+    if sfCheck(hand7):
+        out = "straight flush"
     return out
-def runGameTester():
-    #print(table)
-    return a
-def resTest(nameOfResult): # lowercase eg "one pair" (see resCheck)
-    # testing for a pair
-    out = False
-    gcount = 1
-    res = ""
-    global mutableOrderedDeck
-    while not out:
-        table.clear() # clear table
-        dealt.clear() # get rid of the cards
-        mutableOrderedDeck.clear()
-        mutableOrderedDeck = deck.copy()
-        addRobot("GENERATE")
-        deal()
-        deal()
-        deal()
-        a = resCheck()
-        out = a.count(nameOfResult)
-        out = out > 0
-        gcount = gcount + 1
-    for p in table:
-        p.showCards()
-    print("\n" + str(dealt))
-    print("Game "+ str(gcount))
-    #print(" ".join(dealt))
+def show():
+    for x in table:
+        x.showCards()
+def flushSuit(sevenCards):
+    # there must be mostly this suit, so that reduces the problem massively
+    suits = ["S","H","C","D"]
+    countS = [0]*len(suits)
+    idx = 0
+    for suit in suits:
+        for cardElement in sevenCards:
+            if suit==cardElement:
+                countS[idx] += 1
+        idx += 1
+    fSuit = -1
+    for a in range(0,len(suits)):
+        if countS[a] == 5:
+            fSuit = a
+    if fSuit == -1:
+        return ""
+    else:
+        return suits[fSuit]
+def sfCheck(sevenCards):
+    suit = flushSuit(sevenCards)
+    usedCardVals = straightCheck(sevenCards)
+    outCount = 0
+    idx = 0
+    for cardElement in sevenCards:
+        for cardVal in usedCardVals:
+            if cardVal == cardElement:
+                # the suit will be to its right
+                if sevenCards[idx + 1] == suit:
+                    outCount = outCount + 1
+        idx += 1
+    return outCount == 5            
